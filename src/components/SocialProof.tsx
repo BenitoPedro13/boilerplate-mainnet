7173d7b96de47b335fed0115c7733667b8d5b442
Benito Pedro <benitopedro21@outlook.com>
2023-06-09 15:13:20 -0300

    Social Proof responsive section

--- src/components/SocialProof.tsx
+++ src/components/SocialProof.tsx
@@ -9,36 +9,30 @@ import companyLogo from '@/../public/company-logo.svg'
 
 const SocialProof = () => {
   return (
-    <section className="flex flex-row justify-center items-start pb-16 w-full h-fit bg-white">
-      <div className="flex flex-col items-start px-4 gap-8 w-full h-fit">
+    <section className="flex flex-row justify-center items-start pb-16 w-full h-fit bg-white lg:py-24">
+      <div className="flex flex-col items-start px-4 gap-8 w-full h-fit lg:px-8 max-w-7xl">
         <p className="font-medium text-base text-center text-gray-600 w-full">
           Join 4,000+ companies already growing
         </p>
-        <div className="flex flex-col items-start p-0 gap-4 w-full h-fit">
-          <div className="flex flex-row justify-center items-center p-0 gap-8 w-full h-fit">
+        <div className="flex flex-row items-start p-0 gap-4 w-full h-fit justify-center lg:gap-8 flex-wrap">
             <div className="w-fit h-fit">
               <Image src={companyLogo} height={36} width={undefined} alt="Company Logo"/>
             </div>
             <div className="w-fit h-fit">
               <Image src={companyLogo1} height={36} width={undefined} alt="Company Logo"/>
             </div>
-          </div>
-          <div className="flex flex-row justify-center items-center p-0 gap-8 w-full h-fit">
             <div className="w-fit h-fit">
               <Image src={companyLogo2} height={36} width={undefined} alt="Company Logo"/>
             </div>
             <div className="w-fit h-fit">
               <Image src={companyLogo3} height={36} width={undefined} alt="Company Logo"/>
             </div>
-          </div>
-          <div className="flex flex-row justify-center items-center p-0 gap-8 w-full h-fit">
             <div className="w-fit h-fit">
               <Image src={companyLogo4} height={36} width={undefined} alt="Company Logo"/>
             </div>
             <div className="w-fit h-fit">
               <Image src={companyLogo5} height={36} width={undefined} alt="Company Logo"/>
             </div>
-          </div>
         </div>
       </div>
     </section>
