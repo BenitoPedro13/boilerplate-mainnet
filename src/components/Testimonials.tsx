dfa7329f32bc6ad0ce37a809c43827d678cb31c2
Benito Pedro <benitopedro21@outlook.com>
2023-06-09 19:04:37 -0300

    Testimonial responsive section

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -9,12 +9,12 @@ import testimonialAvatar from '@/../public/testimonial-avatar.png'
 
 const Testimonials = () => {
   return (
-    <section className="flex flex-col items-center py-16 gap-16 w-full h-fit bg-gray-50">
-      <div className="flex flex-col items-start px-4 gap-8 w-full h-fit">
+    <section className="flex flex-col items-center py-16 gap-16 w-full h-fit bg-gray-50 lg:py-24">
+      <div className="flex flex-col items-start px-4 gap-8 w-full h-fit lg:px-8 lg:gap-8 max-w-7xl">
         <div className="flex flex-col items-center p-0 gap-10 w-full h-fit">
           <div className="flex flex-col items-center gap-8 w-full h-fit">
             <Image src={companyLogo6} width={undefined} height={40} alt="Company Logo"/>
-            <h3 className="w-full h-fit font-medium text-3xl leading-[38px] text-center text-gray-900">
+            <h3 className="w-full h-fit font-medium text-3xl leading-[38px] text-center text-gray-900 lg:text-5xl lg:leading-[60px] ">
               We’ve been using Untitled to kick start every new project and can’t imagine working without it.
             </h3>
             <div className="flex flex-col items-center gap-4 w-full h-fit">
