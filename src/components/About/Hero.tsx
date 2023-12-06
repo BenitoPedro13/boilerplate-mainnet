b956186efd59761943ed7f9c3ab5346e5b35ae62
Benito <benitopedro21@outlook.com>
2023-12-06 17:06:17 -0300

    Co-authored-by: PDR77 <PDR7700@users.noreply.github.com>

--- src/components/About/Hero.tsx
+++ src/components/About/Hero.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 const AboutUsHeroSection = () => {
+  const [color, setColor] = useState("#7a2323");
+
   return (
     <section className="flex flex-col items-center gap-16 py-16 px-0 self-stretch bg-white">
       <div className="flex items-start py-0 px-4 self-stretch">
@@ -10,7 +13,17 @@ const AboutUsHeroSection = () => {
               <p className="self-stretch text-primary-700 text-center text-sm font-semibold">
                 About us
               </p>
-              <h1 className="text-gray-900 text-center text-4xl leading-[44px] font-semibold tracking-[-0.72px] self-stretch">
+              <h1
+                onMouseEnter={() => {
+                  if (color === "#7a2323") {
+                    return setColor("#ff0067");
+                  }
+                  return setColor("#7a2323");
+                }}
+                className={`${
+                  color === "#7a2323" ? "scale-125" : "scale-100"
+                } transition-all duration-300 text-center text-[${color}] text-4xl leading-[44px] font-semibold tracking-[-0.72px] self-stretch`}
+              >
                 About the company
               </h1>
             </div>
